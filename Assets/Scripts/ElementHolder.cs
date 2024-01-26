f3bc2af4f6280562d72dc7346f70a849c61dd496
vgilabert <vincent.gilabert@gmail.com>
2024-01-26 10:28:25 +0100

    Allow more objects in holders

--- Assets/Scripts/ElementHolder.cs
+++ Assets/Scripts/ElementHolder.cs
@@ -1,76 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class ElementHolder : MonoBehaviour
 {
     public ElementObject element;
     public Vector3 spawnOffset;
-    public float overlapSphereRadius = 0.5f;
-    public float timeToLive = 5f; // how long the object can be out of the holder before it is destroyed
-    private float liveTimer; // how long the object will take to respawn after it returns to the holder
+    public Collider containerCheckCollider;
+    public float elementNb = 3; // how many elements can be in the holder at the same time
     
-    private GrabObject _elementObject;
-    private bool isOutOfHolder;
-
     void Start()
     {
-        liveTimer = 0;
         if (element.prefab != null)
         {
-            _elementObject = Instantiate(element.prefab, transform.position + spawnOffset, Quaternion.identity, transform);
-
+            for (int i = 0; i < elementNb; i++)
+            {
+                Instantiate(element.prefab, transform.position + spawnOffset, Quaternion.identity, transform);
+            }
         }
     }
 
-    void Update()
+    public void Respawn(GrabObject grabObject)
     {
-        CheckElementObjectPosition();
-    }
-
-    private void CheckElementObjectPosition()
-    {
-        isOutOfHolder = false;
-        // Check if the element object is out of the holder with an overlap sphere
-        Collider[] colliders = Physics.OverlapSphere(transform.position + spawnOffset, overlapSphereRadius);
-        if (colliders.Any(col => col.gameObject == _elementObject.gameObject) == false)
-        {
-            isOutOfHolder = true;
-        }
-        if (isOutOfHolder && _elementObject.isGrabbed == false)
-        {
-            liveTimer += Time.deltaTime;
-        }
-        else
-        {
-            liveTimer = 0;
-        }
-        if (_elementObject.transform.position.y < - 5)
-        {
-            Respawn();
-            DialogueManager.Instance.TriggerDialogue(DialogueType.IngredientDropped);
-        }
-        if (liveTimer > timeToLive)
-        {
-            Respawn();
-        }
-    }
-
-    private void Respawn()
-    {
-        _elementObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        _elementObject.transform.position = transform.position + spawnOffset;
+        grabObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        grabObject.transform.position = transform.position + spawnOffset;
     }
 
     void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position + spawnOffset, 0.2f);
-        // Draw a wire sphere to show the overlap sphere in red
-        if (isOutOfHolder)
-            Gizmos.color = Color.red;
-        else 
-            Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position + spawnOffset, overlapSphereRadius);
     }
 }
--- Assets/Scripts/GrabObject.cs
+++ Assets/Scripts/GrabObject.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 public class GrabObject : MonoBehaviour
 {
     private GrabManager grabManager;
-
-    
     private Rigidbody _rigidbody;
+    
     public bool isGrabbed;
-
+    public bool isOutOfHolder;
+    public float timeToLive = 5f;
+    
+    private float liveTimer;
+    private ElementHolder holderReference;
+    
+    
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         grabManager = GrabManager.Instance;
+        holderReference = GetComponentInParent<ElementHolder>();
+        
+        liveTimer = 0;
     }
 
     private void Update()
     {
         UpdatePosition();
+        CheckPosition();
     }
 
     private void UpdatePosition()
@@ -31,6 +40,27 @@ public class GrabObject : MonoBehaviour
         else
         {
             _rigidbody.drag = 0;
+        }
+    }
+
+    void CheckPosition()
+    {
+        isOutOfHolder = !holderReference.containerCheckCollider.bounds.Contains(transform.position);
+        if (isOutOfHolder && !isGrabbed)
+        {
+            liveTimer += Time.deltaTime;
+            if (liveTimer >= timeToLive)
+            {
+                holderReference.Respawn(this);
+            }
+        }
+        else
+        {
+            liveTimer = 0;
+        }
+        if (transform.position.y < -8)
+        {
+            holderReference.Respawn(this);
         }
     }
 
